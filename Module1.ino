92e85d33bd66c6ecc2d8845fd87409a2610635c5
Kev <km3628@columbia.edu>
2024-05-10 09:40:19 -0400

    small update for upload

--- Module1.ino
+++ Module1.ino
@@ -9,8 +9,22 @@
 TFT_eSPI tft = TFT_eSPI();
 
 
+String responses[] = {
+  "Because I'm a circle, and you're an ellipse!",
+  "Because I'm just too round for you.",
+  "Because you're not in my circumference of love.",
+  "Because I'm more into squares.",
+  "Because you haven't circled back to me yet.",
+  "Because I'm a bit too geometric for feelings.",
+  "Because I'm too busy rolling around to settle down.",
+  "Because you're more of an acute angle, and I'm more obtuse.",
+  "Because I'm more into pie than love triangles.",
+  "Because I'm still figuring out my tangents in life.",
+};
 int timeSince = 0;
 int rotation = 1;
+int responseIndex = random(sizeof(responses) / sizeof(responses[0]));
+String chosenResponse = responses[responseIndex];
 void setup(void) {
   // put your setup code here, to run once:
   tft.init();
@@ -46,7 +60,8 @@ void drawMovingText(int setRotation) {
   tft.setTextColor(TFT_BLACK, TFT_BLACK);
   tft.drawEllipse(220, 10, 30, 30, TFT_BLUE);
   tft.fillEllipse(220, 10, 30, 30, TFT_BLUE);
-  tft.drawString("Because I'm too high up to love you!", 5, 64, 2);
+  tft.setTextSize(calculateTextSize(jokeResponse));
+  tft.drawString(chosenResponse, 5, 64, 2);
   delay(DELAY);
 
   tft.fillScreen(TFT_RED);
@@ -54,3 +69,17 @@ void drawMovingText(int setRotation) {
   tft.drawString("A Love Story about Two Circles", 15, 64, 2);
   delay(5000);
 }
+
+int calculateTextSize(String text) {
+  int textSize = 1;
+  int textLength = text.length();
+  if (textLength > 10) {
+    textSize = 1;
+  } else if (textLength > 5) {
+    textSize = 2;
+  } else {
+    textSize = 3;
+  }
+  return textSize;
+}
+
